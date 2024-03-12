7ef076399df2620d19e6d1aa78ed4936063a8867
Joan Amal <Joan.Amalorpavam@sight-sound.com>
2024-03-12 01:31:36 -0400

    switch the column tax and item total

--- app/src/app/dashboard/order/[id]/receipt.tsx
+++ app/src/app/dashboard/order/[id]/receipt.tsx
@@ -99,28 +99,28 @@ export const Receipt = ({ order }: { order: any }) => {
                 <Text style={styles.quantityTableCell}>Qty.</Text>
                 <Text style={styles.itemCodeTableCell}>Item Code</Text>
                 <Text style={styles.tableCell}>Unit Price</Text>
-                <Text style={styles.tableCell}>Item Total</Text>
                 <Text style={styles.lastTableCell}>Tax</Text>
+                <Text style={styles.tableCell}>Item Total</Text>
               </View>
               {order.items.map((item: any) => (
                   <View key={item.id} style={styles.tableRow}>
                     <Text style={styles.quantityTableCell}>{item.quantity}</Text>
                     <Text style={styles.itemCodeTableCell}>{item.product.name}</Text>
                     <Text style={styles.tableCell}>Rs.{item.price.toFixed(2)}</Text>
-                    <Text style={styles.tableCell}>Rs.{(item.price * item.quantity).toFixed(2)}</Text>
                     <Text style={styles.lastTableCell}>
                       Rs.{((item.price * item.quantity) * (item.product.cgstTaxRate + item.product.sgstTaxRate)).toFixed(2)}
                       {"\n"}
                       (C {item.product.cgstTaxRate * 100}% S {item.product.sgstTaxRate * 100}%)
                     </Text>
+                    <Text style={styles.tableCell}>Rs.{(item.price * item.quantity).toFixed(2)}</Text>
                   </View>
               ))}
               <View style={[styles.totalRow, { borderBottomWidth: 0 }]}>
                 <Text style={[styles.quantityTableCell, styles.totalText]}></Text>
                 <Text style={[styles.itemCodeTableCell, styles.totalText]}></Text>
                 <Text style={[styles.tableCell, styles.totalText]}></Text>
-                <Text style={[styles.tableCell, styles.totalText]}>Rs.{totalItemTotal.toFixed(2)}</Text>
                 <Text style={[styles.lastTableCell, styles.totalText]}>Rs.{totalTax.toFixed(2)}</Text>
+                <Text style={[styles.tableCell, styles.totalText]}>Rs.{totalItemTotal.toFixed(2)}</Text>
               </View>
             </View>
           </View>
